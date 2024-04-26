99072d8b56d0af43e29f7ce2d2666e9ebefee36f
Aaron Griffith <aargri@gmail.com>
2024-04-26 14:17:29 -0400

    --plain-file doesn't make much sense anymore

--- k5tool/src/common.rs
+++ k5tool/src/common.rs
@@ -5,15 +5,12 @@ pub struct SerialPortArgs {
     #[arg(short, long, default_value_t = k5lib::protocol::BAUD_RATE)]
     baud: u32,
     #[arg(long)]
-    plain_file: bool,
-    #[arg(long)]
     tcp: bool,
 }
 
 #[derive(Debug)]
 pub enum SerialPort {
     Serial(std::io::BufWriter<Box<dyn serialport::SerialPort>>),
-    File(std::io::BufWriter<std::fs::File>),
     Tcp(std::io::BufWriter<std::net::TcpStream>),
 }
 
@@ -45,7 +42,6 @@ impl std::io::Read for SerialPort {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         match self {
             Self::Serial(port) => port.get_mut().read(buf),
-            Self::File(port) => port.get_mut().read(buf),
             Self::Tcp(port) => port.get_mut().read(buf),
         }
     }
@@ -55,7 +51,6 @@ impl std::io::Write for SerialPort {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         match self {
             Self::Serial(port) => port.write(buf),
-            Self::File(port) => port.write(buf),
             Self::Tcp(port) => port.write(buf),
         }
     }
@@ -63,7 +58,6 @@ impl std::io::Write for SerialPort {
     fn flush(&mut self) -> std::io::Result<()> {
         match self {
             Self::Serial(port) => port.flush(),
-            Self::File(port) => port.flush(),
             Self::Tcp(port) => port.flush(),
         }
     }
@@ -74,13 +68,6 @@ impl SerialPortArgs {
         if self.tcp {
             let port = std::net::TcpStream::connect(&self.port)?;
             Ok(SerialPort::Tcp(std::io::BufWriter::new(port)))
-        } else if self.plain_file {
-            let port = std::fs::File::options()
-                .read(true)
-                .write(true)
-                .open(&self.port)?;
-
-            Ok(SerialPort::File(std::io::BufWriter::new(port)))
         } else {
             let mut port = serialport::new(&self.port, self.baud).open()?;
             port.set_timeout(std::time::Duration::from_secs(1))?;
