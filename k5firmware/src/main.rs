86676b9e78395e7f56390f7e27f07a906935f747
Aaron Griffith <aargri@gmail.com>
2024-05-18 21:18:36 -0400

    lcd cleanup, snake

--- k5firmware/src/main.rs
+++ k5firmware/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
+
 use dp32g030_hal as hal;
 use panic_halt as _;
 
@@ -27,68 +29,34 @@ impl embedded_hal_02::digital::v2::InputPin for NoPin {
 
 struct DisplaySpec;
 
-impl st7565::DisplaySpecs<128, 64, 8> for DisplaySpec {
-    // original firmware initialization sequence:
-
-    // 0xE2: Reset
-
-    // delay 120 ms
-
-    // 0xA2: LCD Bias Set 0 (BIAS_MODE_1 = false)
-    // 0xC0: Common Mode Output Select Normal (ROW_FLIP = false)
-    // 0xA1: ADC Select Reverse (COLUMN_FLIP = true)
-    // 0xA6: Display Normal (INVERTED = false)
-    // 0xA4: Display All Points Off
-    // 0x24: Voltage Internal Resistor Ratio set to 0x4
-    // 0x81: Electronic Volume Mode Set Byte 1
-    // 0x1F: Electronic Volume Mode Set Byte 2, set to 0x1f
-    // 0x2B: 0b0010 0b1011 booster off, regulator on, follower on
-
-    // delay 1 ms
-
-    // 0x2E: 0b0010 0b1110 booster on, regulator on, follower off
-
-    // delay 1 ms
-
-    // 0x2F: 0b0010 0b1111 booster on, regulator on, follower on
-    // 0x2F: **
-    // 0x2F: **
-
-    // delay 40 ms
-
-    // 0x40: Display Line Start Set 0
-    // 0xAF: Display ON
-
-    // end of sequence
-
-    // 0xC0 | (flip_rows << 3), send 0xC0 means false
+impl st7565::DisplaySpecs<132, 64, 8> for DisplaySpec {
+    // 0xC0
     const FLIP_ROWS: bool = false;
 
-    // 0xA0 | flip_columns, send 0xA1 means true
+    // 0xA1
     const FLIP_COLUMNS: bool = true;
 
-    // 0xA6 | inverted, send 0xA6 means false
+    // 0xA6
     const INVERTED: bool = false;
 
-    // 0xA2 | bias_mode_1, send 0xA2 means false
+    // 0xA2
     const BIAS_MODE_1: bool = false;
 
-    // 0x28 | (booster << 2) | (regulator << 1) | follower
-    // send 0x2f means booster, regulator, follower all 1
+    // 0x2f
     const POWER_CONTROL: st7565::types::PowerControlMode = st7565::types::PowerControlMode {
         booster_circuit: true,
         voltage_regulator_circuit: true,
         voltage_follower_circuit: true,
     };
 
-    // 0x20 | (resistor_ratio & 0b111), send 0x24 means 0x4
+    // 0x24
     const VOLTAGE_REGULATOR_RESISTOR_RATIO: u8 = 0x4;
 
-    // 0x81 (volume & 0b0011_1111), send 0x81 0x1f means 0x1f
+    // 0x81 0x1f
     const ELECTRONIC_VOLUME: u8 = 0x1f;
 
-    // 0xF8 booster_ratio, send .. nothing. hm.
     // this appears to be an internal command??
+    // it's not present in original firmware
     // go with the most 0 one
     const BOOSTER_RATIO: st7565::types::BoosterRatio = st7565::types::BoosterRatio::StepUp2x3x4x;
 }
@@ -161,9 +129,9 @@ fn main() -> ! {
         .unwrap()
         .port(uart_rx, uart_tx);
 
-    // get a timer going at 100kHz for delays and i2c
-    let timer100k = hal::timer::new(p.TIMER_BASE0, power.gates.timer_base0)
-        .frequency::<{ Hertz::kHz(100).to_Hz() }>(&clocks)
+    // get a timer going at 200kHz i2c
+    let timer200k = hal::timer::new(p.TIMER_BASE0, power.gates.timer_base0)
+        .frequency::<{ Hertz::kHz(200).to_Hz() }>(&clocks)
         .unwrap()
         .split(&clocks);
 
@@ -175,18 +143,22 @@ fn main() -> ! {
         .unwrap()
         .split(&clocks);
 
-    // bitbang eeprom i2c at 50kHz (half the timer frequency)
-    let mut i2c_timer = timer100k.low.timing();
-    i2c_timer.start_rate(100.kHz()).unwrap();
+    // get a timer going at 1kHz for delays
+    let timer1k = hal::timer::new(p.TIMER_BASE2, power.gates.timer_base2)
+        .frequency::<{ Hertz::kHz(1).to_Hz() }>(&clocks)
+        .unwrap()
+        .split(&clocks);
+    let mut delay = timer1k.low.timing();
+
+    // bitbang eeprom i2c at 100kHz (half the timer frequency)
+    let mut i2c_timer = timer200k.low.timing();
+    i2c_timer.start_native().unwrap();
     let i2c = bitbang_hal::i2c::I2cBB::new(eeprom_scl, eeprom_sda, i2c_timer);
     let mut eeprom = eeprom24x::Eeprom24x::new_24x64(i2c, eeprom24x::SlaveAddr::default());
 
-    // delay timer
-    let mut delay = timer100k.high.timing();
-
     // bitbang spi at 500kHz (half the timer frequency)
     let mut spi_timer = timer1m.low.timing();
-    spi_timer.start_rate(1.MHz()).unwrap();
+    spi_timer.start_native().unwrap();
     let spi = bitbang_hal::spi::SPI::new(
         bitbang_hal::spi::MODE_3,
         NoPin,
@@ -201,6 +173,21 @@ fn main() -> ! {
     let mut lcd =
         st7565::ST7565::new(lcd_interface, DisplaySpec).into_graphics_mode(&mut page_buffer);
     lcd.reset(&mut lcd_res, &mut delay).unwrap();
+    let mut lcd = {
+        // set line to 0, this isn't exposed in a way we can access
+        // lcd.set_line_offset(0).unwrap();
+        let (lcd, di) = lcd.release_display_interface();
+        let (mut spi, mut dc, mut cs) = di.release();
+        use embedded_hal_02::blocking::spi::Write;
+
+        cs.set_low();
+        dc.set_low();
+        spi.write(&[0b01000000]).unwrap();
+        cs.set_high();
+
+        let di = display_interface_spi::SPIInterface::new(spi, dc, cs);
+        lcd.attach_display_interface(di)
+    };
     lcd.flush().unwrap();
     lcd.set_display_on(true).unwrap();
 
@@ -239,47 +226,95 @@ fn main() -> ! {
     // turn on backlight
     backlight.set_high();
 
-    // set the timer to complete every 500ms
-    delay.start(500.millis()).unwrap();
+    let mut led_blink = delay;
+    led_blink.start_frequency(2.Hz()).unwrap();
+
+    let mut update_display = timer1k.high.timing();
+    update_display.start_frequency(30.Hz()).unwrap();
 
     // a buffer in which to store our serial data
     let mut line_buf = [0; 0x100];
     let mut line_size = 0;
 
+    // a snaking dot that moves across the screen
+    let mut snake = 0;
+    const SNAKE_LEN: i32 = 50;
+
+    // calculate a Point based on a snake value
+    fn snake_point(border: Rectangle, mut snake: i32) -> Point {
+        let (left, top) = border.top_left.into();
+        let (width, height) = border.size.into();
+        let width = width as i32 - 1;
+        let height = height as i32 - 1;
+        let right = left + width;
+        let bottom = top + height;
+
+        // why are the first 4 columns hidden
+        let left = left + 4;
+        let width = width - 4;
+
+        snake %= 2 * width + 2 * height;
+
+        if snake < width {
+            Point::new(left + snake, top)
+        } else if snake < width + height {
+            snake -= width;
+            Point::new(right, top + snake)
+        } else if snake < 2 * width + height {
+            snake -= width + height;
+            Point::new(right - snake, bottom)
+        } else {
+            snake -= 2 * width + height;
+            Point::new(left, bottom - snake)
+        }
+    }
+
     loop {
-        // ptt pressed means ptt low
-        // ptt pressed means toggle flashlight
-        if ptt.is_low() {
-            flashlight.toggle();
+        if let Ok(()) = led_blink.wait() {
+            // ptt pressed means ptt low
+            // ptt pressed means toggle flashlight
+            if ptt.is_low() {
+                flashlight.toggle();
+            }
         }
 
-        // handle serial until the timer expires
-        use core::fmt::Write;
-        while !matches!(delay.wait(), Ok(())) {
-            if let Ok(c) = uart.rx.read_one() {
-                if c == b'\r' {
-                    uart.tx.write_all(b"\r\n".as_ref()).unwrap();
-                    let line = core::str::from_utf8(&line_buf[..line_size]);
-                    line_size = 0;
+        if let Ok(()) = update_display.wait() {
+            snake += 1;
+            Pixel(snake_point(lcd.bounding_box(), snake), BinaryColor::On)
+                .draw(&mut lcd)
+                .unwrap();
+            Pixel(
+                snake_point(lcd.bounding_box(), snake - SNAKE_LEN),
+                BinaryColor::Off,
+            )
+            .draw(&mut lcd)
+            .unwrap();
+            lcd.flush().unwrap();
+        }
 
-                    // read the given address
-                    if let Some(addr) = line.ok().and_then(|s| u32::from_str_radix(s, 16).ok()) {
-                        let mut eeprom_data = [0; 16];
-                        eeprom.read_data(addr, &mut eeprom_data[..]).unwrap();
-                        writeln!(uart.tx, "eeprom data: {:x?}", eeprom_data).unwrap();
-                    }
-                    continue;
+        if let Ok(c) = uart.rx.read_one() {
+            if c == b'\r' {
+                uart.tx.write_all(b"\r\n".as_ref()).unwrap();
+                let line = core::str::from_utf8(&line_buf[..line_size]);
+                line_size = 0;
+
+                // read the given address
+                if let Some(addr) = line.ok().and_then(|s| u32::from_str_radix(s, 16).ok()) {
+                    let mut eeprom_data = [0; 16];
+                    eeprom.read_data(addr, &mut eeprom_data[..]).unwrap();
+                    writeln!(uart.tx, "eeprom data: {:x?}", eeprom_data).unwrap();
                 }
-
-                if line_size >= line_buf.len() {
-                    writeln!(uart.tx, "overrun!").unwrap();
-                    line_size = 0;
-                }
-
-                uart.tx.write_all(&[c]).unwrap();
-                line_buf[line_size] = c;
-                line_size += 1;
+                continue;
             }
+
+            if line_size >= line_buf.len() {
+                writeln!(uart.tx, "overrun!").unwrap();
+                line_size = 0;
+            }
+
+            uart.tx.write_all(&[c]).unwrap();
+            line_buf[line_size] = c;
+            line_size += 1;
         }
     }
 }
