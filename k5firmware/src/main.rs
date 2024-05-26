88d3c2b0d8654b566351578b1fcda5141b143c14
Aaron Griffith <aargri@gmail.com>
2024-05-26 08:25:04 -0400

    global uart output with lock, catch and print errors in main

--- k5firmware/src/main.rs
+++ k5firmware/src/main.rs
@@ -20,6 +20,26 @@ hal::version!(env!("CARGO_PKG_VERSION"));
 static ALLOCATOR: alloc_cortex_m::CortexMHeap = alloc_cortex_m::CortexMHeap::empty();
 const HEAP_SIZE: usize = 1024;
 
+static UART_RX: spin::Once<spin::Mutex<hal::uart::Rx<hal::pac::UART1>>> = spin::Once::new();
+static UART_TX: spin::Once<spin::Mutex<hal::uart::Tx<hal::pac::UART1>>> = spin::Once::new();
+
+macro_rules! println {
+    () => (print!("\n"));
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
+
+macro_rules! print {
+    ($($arg:tt)*) => {
+        // only try to lock.. if it's locked, it's likely never to unlock
+        // this is a best effort deal
+        if let Some(mutex) = UART_TX.get() {
+            if let Some(mut guard) = mutex.try_lock() {
+                write!(guard, "{}", format_args!($($arg)*)).unwrap();
+            }
+        }
+    }
+}
+
 struct NoPin;
 
 impl embedded_hal_02::digital::v2::InputPin for NoPin {
@@ -71,8 +91,70 @@ impl st7565::DisplaySpecs<128, 64, 8> for DisplaySpec {
     const COLUMN_OFFSET: u8 = 4;
 }
 
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct StringError(alloc::string::String);
+
+impl StringError {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+impl core::fmt::Display for StringError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
+
+impl<E> From<E> for StringError
+where
+    E: core::fmt::Debug,
+{
+    fn from(value: E) -> Self {
+        StringError(alloc::format!("{:?}", value))
+    }
+}
+
+impl core::ops::Deref for StringError {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_str()
+    }
+}
+
 #[cortex_m_rt::entry]
 fn main() -> ! {
+    match go() {
+        Ok(()) => loop {
+            cortex_m::asm::wfi();
+        },
+        Err(e) => loop {
+            println!("error in go(), press any key to reset");
+            println!("{}", e);
+
+            // reset if character received
+            if let Some(mutex) = UART_RX.get() {
+                if let Some(mut guard) = mutex.try_lock() {
+                    if guard.read_one().is_ok() {
+                        println!("!!! reset !!!");
+                        if let Some(txmutex) = UART_TX.get() {
+                            if let Some(mut txguard) = txmutex.try_lock() {
+                                hal::block::block!(txguard.flush()).unwrap();
+                            }
+                        }
+                        cortex_m::peripheral::SCB::sys_reset();
+                    }
+                }
+            }
+
+            // don't spam the uart
+            cortex_m::asm::delay(10_000_000);
+        },
+    }
+}
+
+fn go() -> Result<(), StringError> {
     unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
 
     let p = hal::pac::Peripherals::take().unwrap();
@@ -85,17 +167,20 @@ fn main() -> ! {
     let pins_b = ports.port_b.enable(power.gates.gpio_b);
     let pins_c = ports.port_c.enable(power.gates.gpio_c);
 
+    // fast track the uart (tx A7, rx A8)
+    let uart_tx = pins_a.a7.into();
+    let uart_rx = pins_a.a8.into();
+    let uart =
+        hal::uart::new(p.UART1, power.gates.uart1, &clocks, 38_400.Hz())?.port(uart_rx, uart_tx);
+    UART_RX.call_once(|| uart.rx.into());
+    UART_TX.call_once(|| uart.tx.into());
+
     // PA3 keypad column 1
     let col1 = pins_a.a3.into_pull_up_input();
     // PA4 keypad column 2
     let col2 = pins_a.a4.into_pull_up_input();
     // PA5 keypad column 3
     // PA6 keypad column 4
-
-    // PA7 uart1 tx
-    let uart_tx = pins_a.a7.into();
-    // PA8 uart1 rx
-    let uart_rx = pins_a.a8.into();
 
     // PA9 battery voltage
 
@@ -140,29 +225,22 @@ fn main() -> ! {
     // PC5 ptt
     let ptt = pins_c.c5.into_pull_up_input();
 
-    // initialize uart
-    let mut uart = hal::uart::new(p.UART1, power.gates.uart1, &clocks, 38_400.Hz())
-        .unwrap()
-        .port(uart_rx, uart_tx);
-
     // get a timer going at 200kHz i2c
     let timer200k = hal::timer::new(p.TIMER_BASE0, power.gates.timer_base0)
-        .frequency::<{ Hertz::kHz(200).to_Hz() }>(&clocks)
-        .unwrap()
+        .frequency::<{ Hertz::kHz(200).to_Hz() }>(&clocks)?
         .split(&clocks);
     let mut delay = timer200k.high.timing();
 
     // get a timer going at 1kHz for blinks and frames
     let timer1k = hal::timer::new(p.TIMER_BASE2, power.gates.timer_base2)
-        .frequency::<{ Hertz::kHz(1).to_Hz() }>(&clocks)
-        .unwrap()
+        .frequency::<{ Hertz::kHz(1).to_Hz() }>(&clocks)?
         .split(&clocks);
 
     // bitbang eeprom i2c at 100kHz (half the timer frequency)
     let mut i2c_timer = timer200k.low.timing();
-    i2c_timer.start_native().unwrap();
+    i2c_timer.start_native()?;
     let mut i2c = bitbang_hal::i2c::I2cBB::new(eeprom_scl, eeprom_sda, i2c_timer);
-    let mut fm = bk1080::Bk1080::new(&mut i2c).unwrap();
+    let mut fm = bk1080::Bk1080::new(&mut i2c)?;
     //let mut eeprom = eeprom24x::Eeprom24x::new_24x64(i2c, eeprom24x::SlaveAddr::default());
 
     // spi for the display
@@ -177,9 +255,9 @@ fn main() -> ! {
     let mut page_buffer = st7565::GraphicsPageBuffer::new();
     let mut lcd =
         st7565::ST7565::new(lcd_interface, DisplaySpec).into_graphics_mode(&mut page_buffer);
-    lcd.reset(&mut lcd_res, &mut delay).unwrap();
-    lcd.flush().unwrap();
-    lcd.set_display_on(true).unwrap();
+    lcd.reset(&mut lcd_res, &mut delay)?;
+    lcd.flush()?;
+    lcd.set_display_on(true)?;
 
     // draw a thing
     use embedded_graphics::mono_font::{ascii::FONT_4X6, ascii::FONT_6X10, MonoTextStyle};
@@ -196,10 +274,9 @@ fn main() -> ! {
         font,
         Alignment::Center,
     )
-    .draw(&mut lcd)
-    .unwrap();
+    .draw(&mut lcd)?;
 
-    lcd.flush().unwrap();
+    lcd.flush()?;
 
     // turn off flashlight
     flashlight.set_low();
@@ -209,17 +286,17 @@ fn main() -> ! {
 
     // turn on radio
     fm_enable.set_low(); // active low
-    fm.enable().unwrap();
+    fm.enable()?;
     speaker_enable.set_high();
 
     let mut freq = 0;
-    fm.tune(freq).unwrap();
+    fm.tune(freq)?;
 
     let mut led_blink = timer1k.low.timing();
-    led_blink.start_frequency(2.Hz()).unwrap();
+    led_blink.start_frequency(2.Hz())?;
 
     let mut update_display = timer1k.high.timing();
-    update_display.start_frequency(30.Hz()).unwrap();
+    update_display.start_frequency(30.Hz())?;
 
     // a buffer in which to store our serial data
     let mut line_buf = [0; 0x100];
@@ -279,13 +356,13 @@ fn main() -> ! {
             // button 1 is pressed
             if col1.is_low() {
                 freq += 1;
-                fm.tune(freq).unwrap();
+                fm.tune(freq)?;
             }
 
             // button 2 is pressed
             if col2.is_low() {
                 freq -= 1;
-                fm.tune(freq).unwrap();
+                fm.tune(freq)?;
             }
         }
 
@@ -293,7 +370,7 @@ fn main() -> ! {
             let text = alloc::format!(
                 "freq {:?} rssi {:04x?}",
                 875 + 2 * freq,
-                0x0000 // fm.read(bk1080::REG_RSSI).unwrap(),
+                0x0000 // fm.read(bk1080::REG_RSSI)?,
             );
 
             let text = Text::with_alignment(
@@ -306,29 +383,26 @@ fn main() -> ! {
             if let Some(last_text) = last_text.take() {
                 last_text
                     .into_styled(PrimitiveStyle::with_fill(BinaryColor::Off))
-                    .draw(&mut lcd)
-                    .unwrap();
+                    .draw(&mut lcd)?;
             }
 
-            text.draw(&mut lcd).unwrap();
+            text.draw(&mut lcd)?;
             last_text = Some(text.bounding_box());
 
             snake += 1;
-            Pixel(snake_point(lcd.bounding_box(), snake), BinaryColor::On)
-                .draw(&mut lcd)
-                .unwrap();
+            Pixel(snake_point(lcd.bounding_box(), snake), BinaryColor::On).draw(&mut lcd)?;
             Pixel(
                 snake_point(lcd.bounding_box(), snake - SNAKE_LEN),
                 BinaryColor::Off,
             )
-            .draw(&mut lcd)
-            .unwrap();
-            lcd.flush().unwrap();
+            .draw(&mut lcd)?;
+            lcd.flush()?;
         }
 
-        if let Ok(c) = uart.rx.read_one() {
+        if let Ok(c) = UART_RX.wait().lock().read_one() {
             if c == b'\r' {
-                uart.tx.write_all(b"\r\n".as_ref()).unwrap();
+                print!("\r\n");
+
                 let line = core::str::from_utf8(&line_buf[..line_size]);
                 line_size = 0;
 
@@ -342,7 +416,7 @@ fn main() -> ! {
 
                 match cmd {
                     ("hello", _) => {
-                        writeln!(uart.tx, "Hello!").unwrap();
+                        println!("Hello!");
                     }
                     ("speaker", state) => {
                         let Some(state) = pin_state(state) else {
@@ -357,22 +431,22 @@ fn main() -> ! {
                         fm_enable.set_high();
                     }
                     ("fm", "init") => {
-                        writeln!(uart.tx, "init: {:x?}", fm.enable()).unwrap();
+                        println!("init: {:x?}", fm.enable());
                     }
                     ("tune", val) => {
                         let Ok(val) = val.parse::<u16>() else {
                             continue;
                         };
-                        writeln!(uart.tx, "tune: {:x?}", fm.tune(val)).unwrap();
+                        println!("tune: {:x?}", fm.tune(val));
                     }
                     ("fm", "") => {
                         let all = fm.update(..);
                         if let Ok(all) = all {
                             for (a, v) in all.iter().enumerate() {
-                                writeln!(uart.tx, "fm {:02x}: {:x?}", a, v).unwrap();
+                                println!("fm {:02x}: {:x?}", a, v);
                             }
                         } else {
-                            writeln!(uart.tx, "fm {:?}", all).unwrap();
+                            println!("fm {:?}", all);
                         }
                     }
                     ("read", addr) => {
@@ -380,8 +454,8 @@ fn main() -> ! {
                             continue;
                         };
                         let mut eeprom_data = [0; 16];
-                        //eeprom.read_data(addr, &mut eeprom_data[..]).unwrap();
-                        writeln!(uart.tx, "eeprom data: {:x?}", eeprom_data).unwrap();
+                        //eeprom.read_data(addr, &mut eeprom_data[..])?;
+                        println!("eeprom data: {:x?}", eeprom_data);
                     }
                     _ => {}
                 }
@@ -389,11 +463,11 @@ fn main() -> ! {
             }
 
             if line_size >= line_buf.len() {
-                writeln!(uart.tx, "overrun!").unwrap();
+                println!("overrun!");
                 line_size = 0;
             }
 
-            uart.tx.write_all(&[c]).unwrap();
+            UART_TX.wait().lock().write_all(&[c])?;
             line_buf[line_size] = c;
             line_size += 1;
         }
